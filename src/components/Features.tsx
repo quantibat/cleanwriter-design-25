a9f411687d110eb04ff695797119da6f6fc66f87
quantibat <dev@quantibat.fr>
2025-03-31 18:10:35 +0200

    Correction on main

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -280,7 +280,6 @@ const Features = () => {
         </div>
 
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mb-16 mx-auto w-[85%]">
-
           <div className="flex flex-col ">
             <h2 className="text-3xl font-bold mb-4 text-white">Négocez et Ajustez</h2>
             <p className="text-blue-100/70 mb-6">
