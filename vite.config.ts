fd7c44c269878a89808545be904b89ec06e3b2ff
quantibat <dev@quantibat.fr>
2025-04-10 10:15:19 +0200

    PWA

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import {VitePWA} from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -13,6 +14,28 @@ export default defineConfig(({ mode }) => ({
     react(),
     mode === 'development' &&
     componentTagger(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'DCE Manager',
+        short_name: 'DCE Manager',
+        description: 'Application de marchés publics',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '192X192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '512X512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
   ].filter(Boolean),
   resolve: {
     alias: {
