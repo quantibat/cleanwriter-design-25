4f033b76b0a1715ae68dd4b7f1a0267836c931ac
quantibat <dev@quantibat.fr>
2025-04-23 16:13:13 +0200

    correction

--- src/pages/TermsOfService.tsx
+++ src/pages/TermsOfService.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 const TermsOfService = () => {
